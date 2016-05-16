013a6feb055a2a2d67faeba1c6e3973523ee6557
Leesanwei <lswei93@hotmail.com>
2016-05-16 23:10:15 +0200

    Update affichecommande.php

--- controller/affichecommande.php
+++ controller/affichecommande.php
@@ -6,6 +6,7 @@
 		echo '<td><img src="'.$rowcommande['image'].'" style="height:70px;width:110px"></td>';
 		echo '<td>'.$rowcommande['nom'].'</td>';
 		echo '<td>'.$rowcommande['prix'].'</td>';
+		echo '<td><button class="btn btn-primary btn-lg" type="submit" name="supprimercommande" value="'.$rowcommande['id_reservation'].'">Supprimer</button></td>';
 		echo '</tr>';
 		$prixtotal = $prixtotal + $rowcommande['prix'];
 	}
